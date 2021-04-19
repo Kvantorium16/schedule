2a6f1c8d5b3774899b79d117e7510ec1a2ea3919
NikitoSss42 <npiterkin@mail.ru>
2021-04-19 10:44:13 +0300

    Add button

--- app/src/main/java/com/example/schedule/MainActivity.kt
+++ app/src/main/java/com/example/schedule/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.schedule
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun onMyButtonClick(view: View?) {
+        Toast.makeText(this, "Зачем вы нажали?", Toast.LENGTH_SHORT).show()
+    }
 }
+
