246119af11a7ea5daed7e8bb3e16117feda2211d
Piter <npiterkin@mail.ru>
2021-05-24 02:19:43 +0300

    Database class

--- app/src/main/java/com/example/schedule/ScheduleDataBase.kt
+++ app/src/main/java/com/example/schedule/ScheduleDataBase.kt
@@ -1,50 +1,94 @@
 package com.example.schedule
 
+
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-
-class ScheduleDataBase(context: Context) : SQLiteOpenHelper(context, "Schedule", null, "DATABASE_VERSION") {
+class ScheduleDataBase(context: Context) : SQLiteOpenHelper(context, "Schedule", null, 1) {
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL("CREATE TABLE Directions(d_id INTEGER PRIMARY KEY AUTOINCREMENT, class_group TEXT, direction TEXT);");
-        db.execSQL("CREATE TABLE Lessons(l_id INTEGER PRIMARY KEY AUTOINCREMENT, class_group TEXT, weekday TEXT, time TEXT);");
+        db.execSQL("CREATE TABLE Directions(d_id INTEGER PRIMARY KEY AUTOINCREMENT, class_group TEXT, direction TEXT);")
+        db.execSQL("CREATE TABLE Lessons(l_id INTEGER PRIMARY KEY AUTOINCREMENT, class_group TEXT, weekday TEXT, time TEXT);")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS Directions")
+        db!!.execSQL("DROP TABLE IF EXISTS Lessons")
+        onCreate(db)
     }
 
-    fun group_add(db: SQLiteDatabase, group_title : String, group_direction : String) {
-        val newValues = ContentValues();
-        newValues.put("class_group", group_title);
-        newValues.put("direction", group_direction);
+
+    fun group_add(group_title: String, group_direction: String) {
+        val db = this.writableDatabase
+        val newValues = ContentValues()
+        newValues.put("class_group", group_title)
+        newValues.put("direction", group_direction)
         val insert = db.insert(
             "Directions",
             null,
             newValues
-        );
+        )
     }
 
-    fun lesson_add(db: SQLiteDatabase, group_title: String, weekday: String, time: String){
-        val newValues = ContentValues();
-        newValues.put("class_group", group_title);
-        newValues.put("direction", weekday);
-        newValues.put("direction", time);
+    fun lesson_add(group_title: String, weekday: String, time: String) {
+        val db = this.writableDatabase
+        val newValues = ContentValues()
+        newValues.put("class_group", group_title)
+        newValues.put("direction", weekday)
+        newValues.put("direction", time)
         val insert = db.insert(
             "Lessons",
             null,
             newValues
-        );
+        )
     }
 
-    fun get_group_list(db: SQLiteDatabase){
-        val cursor: Cursor = db.rawQuery("select class_group from Directions", null);
-        val newValues = ContentValues();
+    fun get_group_list(): ArrayList<String> {
+        val db = this.writableDatabase
+        var cursor: Cursor = db.rawQuery("select class_group from Directions", null)
+        var groups: ArrayList<String> = arrayListOf()
+        if (cursor.moveToFirst()) {
+            do {
+                var group: String = cursor.getString(cursor.getColumnIndex("class_group"))
+                groups.add(group)
 
+            } while (cursor.moveToNext())
+        }
+        return groups
+    }
 
-        newValues.put("class_group", );
-        newValues.put("direction", );
+    fun get_direction_by_group(group: String): String {
+        val db = this.writableDatabase
+        var cursor: Cursor =
+            db.rawQuery("select direction from Directions where class_group='" + group + "'", null)
+        if (cursor.moveToFirst())
+            return cursor.getString(cursor.getColumnIndex("direction"))
+
+        return "not found"
+    }
+
+    fun get_lessons_by_group(group: String): ArrayList<ContentValues> {
+        val db = this.writableDatabase
+        var cursor: Cursor =
+            db.rawQuery("select * from Lessons where class_group='" + group + "'", null)
+        var lessons: ArrayList<ContentValues> = arrayListOf()
+        if (cursor.moveToFirst()) {
+
+            do {
+                var lesson = ContentValues()
+                var id = cursor.getInt(cursor.getColumnIndex("l_id"))
+                var c_group = cursor.getString(cursor.getColumnIndex("class_group"))
+                var weekday = cursor.getString(cursor.getColumnIndex("weekday"))
+                var time = cursor.getString(cursor.getColumnIndex("time"))
+                lesson.put("id", id)
+                lesson.put("class_group", c_group)
+                lesson.put("weekday", weekday)
+                lesson.put("time", time)
+                lessons.add(lesson)
+            } while (cursor.moveToNext())
+        }
+        return lessons
     }
 }
