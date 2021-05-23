0fda67fc295c23ddac702b631a432a12459f5ec8
Desempell <Desempell@yandex.ru>
2021-05-23 20:46:36 +0300

    Interface: New background and card design changed. Add number group.

--- app/src/main/java/com/example/schedule/SecondActivity.kt
+++ app/src/main/java/com/example/schedule/SecondActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.second_layout)
     }
 }
