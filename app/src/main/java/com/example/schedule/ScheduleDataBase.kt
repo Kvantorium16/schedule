d04dd24124de6f340b2b6e0105a7e8f812b42cd9
Piter <npiterkin@mail.ru>
2021-06-20 15:21:31 +0300

    clear tables functions ScheduleDataBase

--- app/src/main/java/com/example/schedule/ScheduleDataBase.kt
+++ app/src/main/java/com/example/schedule/ScheduleDataBase.kt
@@ -31,23 +31,21 @@ class ScheduleDataBase(context: Context) : SQLiteOpenHelper(context, "Schedule",
         values.put("class_group", group_title)
         values.put("direction", group_direction)
         val _success = db.insert("Directions", null, values)
-        db.close()
         return (Integer.parseInt("$_success") != -1)
     }
 
-    fun lesson_add(group_title: String, weekday: String, time: String) : Boolean {
+    fun lesson_add(group_title: String, weekday: String, time: String) {
         val db = this.writableDatabase
         val newValues = ContentValues()
         newValues.put("class_group", group_title)
         newValues.put("weekday", weekday)
         newValues.put("time", time)
-        val _success = db.insert(
+        val insert = db.insert(
             "Lessons",
             null,
             newValues
         )
-        db.close()
-        return (Integer.parseInt("$_success") != -1)
+
     }
 
     fun get_group_list(): ArrayList<String> {
@@ -61,6 +59,7 @@ class ScheduleDataBase(context: Context) : SQLiteOpenHelper(context, "Schedule",
 
             } while (cursor.moveToNext())
         }
+
         return groups
     }
 
@@ -69,6 +68,7 @@ class ScheduleDataBase(context: Context) : SQLiteOpenHelper(context, "Schedule",
         var cursor: Cursor =
             db.rawQuery("select direction from Directions where class_group='" + group + "'", null)
         if (cursor.moveToFirst())
+
             return cursor.getString(cursor.getColumnIndex("direction"))
 
         return "not found"
@@ -94,7 +94,16 @@ class ScheduleDataBase(context: Context) : SQLiteOpenHelper(context, "Schedule",
                 lessons.add(lesson)
             } while (cursor.moveToNext())
         }
+
         return lessons
+    }
+
+    fun clear_directions_table() {
+        this.writableDatabase.execSQL("Delete from Directions")
+    }
+
+    fun clear_lessons_table() {
+        this.writableDatabase.execSQL("Delete from Lessons")
     }
 }
 
