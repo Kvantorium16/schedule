39a001e6e6b3494e99b5eeed897d5d3b5c433949
Jpegga <regan1337@mail.ru>
2021-06-20 14:38:54 +0300

    Fix

--- app/src/main/java/com/example/schedule/HtmlParser.kt
+++ app/src/main/java/com/example/schedule/HtmlParser.kt
@@ -19,68 +19,47 @@ class HtmlParser{
             try {
                 val doc: Document = Jsoup.connect("https://jjpega.pythonanywhere.com/get").get()
 
+                val direction: Elements? = doc.select("td[id=\"direction\"]")
                 var directions: ArrayList<String> = arrayListOf()
-                var i = 0
-                while (true){
-                    try {
-                        val direction: String = doc.select("td[id=\"direction\"]")[i].text()
-                        directions.add(direction)
-                        i += 1
-                    }
-                    catch (e: Exception) {
-                        break
+                if (direction != null) {
+                    for (dir in direction){
+                        directions.add(dir.text())
                     }
                 }
 
                 var num_directions: Int = directions.size
-                i = 0
-                while (i <= num_directions){
-                    var group: String = ""
-                    var groups: ArrayList<String>
-                    try {
-                        var tr: Element? = doc.select("tr[id=\"er\"]")[i]
-                        try {
-                            group = tr!!.select("p[id=\"group\"]").text()
-                            groups = group.split(" ") as ArrayList<String>
-                            groups = groups.distinct() as ArrayList<String>
-                            for (group in groups){
-                                db.group_add(group, directions[i])
-                            }
-                            i += 1
+                var i = 0
+                var group: String = ""
+                var groups: ArrayList<String>
+                var tr: Elements? = doc.select("tr[id=\"er\"]")
+                while (i < num_directions){
+                    group = tr!![i].select("p[id=\"group\"]").text()
+                    if (group != "") {
+                        groups = group.split(" ") as ArrayList<String>
+                        groups = groups.distinct() as ArrayList<String>
+                        for (group in groups) {
+                            db.group_add(group, directions[i])
                         }
-                        catch (e: Exception){
-                            i += 1
-                            continue
-                        }
-
                     }
-                    catch (e: Exception){
-                        break
-                    }
+                    i += 1
                 }
 
-
                 val weekdays: Array<String> = (arrayOf("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"))
-
                 for (days in weekdays) {
                     for (direction in directions) {
                         i = 0
                         val str = "table[id='" + days + "_" + direction + "']"
                         val day: Elements = doc.select(str)
-                        try {
-                            while (true){
-                                db.lesson_add(day.select("p[id=\"group\"]")[i].text(), days, day.select("p[id=\"time\"]")[i].text())
-                                i += 1
-                            }
-                        }
-                        catch (e: Exception){
-                            break
+                        val g: Int = day.select("p[id=\"group\"]").size
+                        while (i < g) {
+                            db.lesson_add(day.select("p[id=\"group\"]")[i].text(), days, day.select("p[id=\"time\"]")[i].text())
+                            i += 1
                         }
                     }
                 }
 
             } catch (e: Exception) {
-
+                println(e)
             }
         }.start()
     }
