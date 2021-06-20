c4377e9c64662957beff44e55f8e624727bc03a6
Jpegga <regan1337@mail.ru>
2021-06-20 11:10:18 +0300

    Correct work parser

--- app/src/main/java/com/example/schedule/HtmlParser.kt
+++ app/src/main/java/com/example/schedule/HtmlParser.kt
@@ -7,38 +7,75 @@ import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 import java.util.*
 import com.example.schedule.ScheduleDataBase.*
+import org.jsoup.nodes.Element
+import kotlin.collections.ArrayList
 
 class HtmlParser{
 
     fun getHtmlFromWeb(db : ScheduleDataBase) {
-        val groups: ArrayList<String> = arrayListOf()
-        val directions: ArrayList<String> = arrayListOf()
         Thread{
+            db.clear_directions_table()
+            db.clear_lessons_table()
             try {
                 val doc: Document = Jsoup.connect("https://jjpega.pythonanywhere.com/get").get()
-                val group: Elements = doc.select("p[id=\"group\"]")
-                for (index in group.indices) {
-                    groups.add(group[index].text())
-                }
-                groups.distinct()
 
-                val direction: Elements = doc.select("td[id=\"direction\"]")
-                for (index in direction.indices) {
-                    directions.add(direction[index].text())
+                var directions: ArrayList<String> = arrayListOf()
+                var i = 0
+                while (true){
+                    try {
+                        val direction: String = doc.select("td[id=\"direction\"]")[i].text()
+                        directions.add(direction)
+                        i += 1
+                    }
+                    catch (e: Exception) {
+                        break
+                    }
                 }
-                directions.distinct()
 
-                for (index in groups.indices) {
-                    db.group_add(groups[index], directions[index])
+                var num_directions: Int = directions.size
+                i = 0
+                while (i <= num_directions){
+                    var group: String = ""
+                    var groups: ArrayList<String>
+                    try {
+                        var tr: Element? = doc.select("tr[id=\"er\"]")[i]
+                        try {
+                            group = tr!!.select("p[id=\"group\"]").text()
+                            groups = group.split(" ") as ArrayList<String>
+                            groups = groups.distinct() as ArrayList<String>
+                            for (group in groups){
+                                db.group_add(group, directions[i])
+                            }
+                            i += 1
+                        }
+                        catch (e: Exception){
+                            i += 1
+                            continue
+                        }
+
+                    }
+                    catch (e: Exception){
+                        break
+                    }
                 }
+
 
                 val weekdays: Array<String> = (arrayOf("Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"))
 
-                for (direction in directions) {
-                    for (days in 0..6) {
-                        val str = "table[id='" + weekdays[days] + "_" + direction + "']"
+                for (days in weekdays) {
+                    for (direction in directions) {
+                        i = 0
+                        val str = "table[id='" + days + "_" + direction + "']"
                         val day: Elements = doc.select(str)
-                        db.lesson_add(day.select("p[id=\"group\"]").text(), weekdays[days], day.select("p[id=\"time\"]").text())
+                        try {
+                            while (true){
+                                db.lesson_add(day.select("p[id=\"group\"]")[i].text(), days, day.select("p[id=\"time\"]")[i].text())
+                                i += 1
+                            }
+                        }
+                        catch (e: Exception){
+                            break
+                        }
                     }
                 }
 
